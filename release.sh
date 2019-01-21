f163c69c86b3a15d9bf7e918097c64ec52e30294
Andrzej Torski <andrew.torski@gmail.com>
2019-01-21 22:28:47 +0100

    Add release script, configure build to use version

--- release.sh
+++ release.sh
@@ -3,8 +3,8 @@ git fetch
 docker run --rm -v "$PWD":/app treeder/bump patch
 version=`cat VERSION`
 echo "version: $version"
+
 git add -A
-git commit -m "version $version"
+git commit -m "Release version $version"
 git tag -a "$version" -m "version $version"
-git push
 git push --tags
